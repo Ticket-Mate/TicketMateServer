5e42717ff4496ef05832a3e63cbd491f5ddd8e3b
MatanShabi <46000126+MatanShabi@users.noreply.github.com>
2024-05-31 09:00:03 +0300

    fix events swagger

--- src/models/event.ts
+++ src/models/event.ts
@@ -1,25 +1,63 @@
 import mongoose, { Schema } from "mongoose";
 
+export type Image = { 
+    url: string
+}
+
+export enum EventStatus {
+    SOLD_OUT='sold out',
+    ON_SALE='on sale',
+    UPCOMING='upcoming',
+    CANCELLED= 'cancelled',
+}
+
 export interface IEvent {
     _id:string,
     name:string,
+    description: string,
+    status: EventStatus,
     type:string,
+    images: Image[]
+    startDate: Date,
+    endDate: Date,
     tickets?:Schema.Types.ObjectId[];
     createdAt: Date,
     updatedAt: Date
-    availableTicket?:Schema.Types.ObjectId[];
-    
+    availableTicket?:Schema.Types.ObjectId[]; 
   }
 
   const eventSchema = new mongoose.Schema<IEvent>({
     name:{
         type:String,
         required:true,
-    } ,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: Object.values(EventStatus),
+        required: true
+    },
     type: {
         type:String,
         requires:true
     },
+    startDate: {
+        type: Date,
+        requires:true
+    },
+    endDate: {
+        type: Date,
+        requires:true
+    },
+    images: [{
+        url: {
+            type: String,
+            requires:true
+        }
+    }],
     tickets: [{
         type: Object,
         ref: 'Ticket'
@@ -36,6 +74,11 @@ export interface IEvent {
     type: Schema.ObjectId,
     ref: 'Ticket'
    }]
+  });
+
+  eventSchema.pre('save', function (next) {
+    this.updatedAt = new Date();
+    next();
   });
 
   export default mongoose.model<IEvent>("Event", eventSchema);
