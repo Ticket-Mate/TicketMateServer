df291973a1afb8e6c16612b131258e77807feb48
AdiMor1999 <adimor87@gmail.com>
2024-08-03 14:46:34 +0300

    add getTicketCountByEventId

--- src/controllers/ticket_controller.ts
+++ src/controllers/ticket_controller.ts
@@ -1,128 +1,158 @@
-import { Request, Response } from 'express';
-import Ticket, { ITicket } from "../models/ticket"
-import Event, { EventStatus } from '../models/event';
-import { notifyUsers } from '../utils/notification';
+import { Request, Response } from "express";
+import Ticket, { ITicket } from "../models/ticket";
+import Event, { EventStatus } from "../models/event";
+import { notifyUsers } from "../utils/notification";
 
 export const getTickets = async (req: Request, res: Response) => {
-    try {
-        const tickets = await Ticket.find();
-        res.status(200).json(tickets);
-    } catch (error) {
-        res.status(500).json({ message: 'Something went wrong getting the tickets' });
-    }
-}
+  try {
+    const tickets = await Ticket.find();
+    res.status(200).json(tickets);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Something went wrong getting the tickets" });
+  }
+};
 
 export const getTicketById = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    try {
-        const ticket = await Ticket.findById({ _id: id });
-        if (!ticket) {
-            return res.status(404).json({ message: 'Ticket not found' });
-        }
-        res.status(200).json(ticket);
-    } catch (error) {
-        res.status(500).json({ message: 'Something went wrong getting the ticket', error });
+  const { id } = req.params;
+  try {
+    const ticket = await Ticket.findById({ _id: id });
+    if (!ticket) {
+      return res.status(404).json({ message: "Ticket not found" });
     }
+    res.status(200).json(ticket);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Something went wrong getting the ticket", error });
+  }
 };
 
 export const createTicket = async (req: Request, res: Response) => {
-    const { barcode, position, originalPrice, resalePrice, ownerId, eventId } = req.body;
-    try {
-        const newTicket = new Ticket({
-            barcode,
-            position,
-            originalPrice,
-            resalePrice,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            ownerId,
-            eventId,
-            onSale: true,
-        });
-        await newTicket.save();
-        if (!newTicket) {
-            console.log("no new ticket");
-            
-            return res.status(404).json({ message: 'Ticket not found' });
-        }
-        console.log("ticket created");
-        await updateEventAvailableTickets(newTicket);
-        res.status(201).json(newTicket);
-    } catch (error) {
-        res.status(500).json({ message: 'Something went wrong creating the ticket', error });
+  const { barcode, position, originalPrice, resalePrice, ownerId, eventId } =
+    req.body;
+  try {
+    const newTicket = new Ticket({
+      barcode,
+      position,
+      originalPrice,
+      resalePrice,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      ownerId,
+      eventId,
+      onSale: true,
+    });
+    await newTicket.save();
+    if (!newTicket) {
+      console.log("no new ticket");
+
+      return res.status(404).json({ message: "Ticket not found" });
     }
+    console.log("ticket created");
+    await updateEventAvailableTickets(newTicket);
+    res.status(201).json(newTicket);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Something went wrong creating the ticket", error });
+  }
 };
 
 export const updateTicket = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const { barcode, position, originalPrice, resalePrice, ownerId, eventId, onSale } = req.body;
-    try {
-        const currentTicket = await Ticket.findById(id);
-        const updatedTicket = await Ticket.findByIdAndUpdate(
-            id,
-            {
-                barcode,
-                position,
-                originalPrice,
-                resalePrice,
-                ownerId,
-                updatedAt: new Date(),
-                eventId,
-                onSale,
-            },
-            { new: true }
-        );
-        if (!updatedTicket) {
-            return res.status(404).json({ message: 'Ticket not found' });
-        }
-        if (currentTicket.onSale != updatedTicket.onSale) {
-            await updateEventAvailableTickets(updatedTicket);
-        }
-        res.status(200).json(updatedTicket);
-    } catch (error) {
-        res.status(500).json({ message: 'Error updating ticket', error });
+  const { id } = req.params;
+  const {
+    barcode,
+    position,
+    originalPrice,
+    resalePrice,
+    ownerId,
+    eventId,
+    onSale,
+  } = req.body;
+  try {
+    const currentTicket = await Ticket.findById(id);
+    const updatedTicket = await Ticket.findByIdAndUpdate(
+      id,
+      {
+        barcode,
+        position,
+        originalPrice,
+        resalePrice,
+        ownerId,
+        updatedAt: new Date(),
+        eventId,
+        onSale,
+      },
+      { new: true }
+    );
+    if (!updatedTicket) {
+      return res.status(404).json({ message: "Ticket not found" });
     }
-}
-
-export const deleteTicket = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    try {
-        const deletedTicket = await Ticket.findByIdAndDelete({ _id: id });
-        if (!deletedTicket) {
-            return res.status(404).json({ message: 'Ticket not found' });
-        }
-        res.status(200).json({ message: 'Ticket deleted successfully' });
-    } catch (error) {
-        res.status(500).json({ message: 'Error deleting ticket', error });
+    if (currentTicket.onSale != updatedTicket.onSale) {
+      await updateEventAvailableTickets(updatedTicket);
     }
+    res.status(200).json(updatedTicket);
+  } catch (error) {
+    res.status(500).json({ message: "Error updating ticket", error });
+  }
 };
 
-export const updateEventAvailableTickets = async (ticket: ITicket) => {    
-    
-    const event = await Event.findById(ticket.eventId);
-    if (ticket.onSale) {
-
-        if (event.status === EventStatus.SOLD_OUT) {
-            
-            await notifyUsers(event.id, event.name)
-            await event.updateOne({ $push: { availableTicket: ticket._id }, status: EventStatus.ON_SALE });
-        }
-        else {
-            await event.updateOne({ $push: { availableTicket: ticket._id } });
-        }
-    } else {
-        await event.updateOne({ $pull: { availableTicket: ticket._id } });
+export const deleteTicket = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const deletedTicket = await Ticket.findByIdAndDelete({ _id: id });
+    if (!deletedTicket) {
+      return res.status(404).json({ message: "Ticket not found" });
     }
-}
+    res.status(200).json({ message: "Ticket deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting ticket", error });
+  }
+};
+
+export const updateEventAvailableTickets = async (ticket: ITicket) => {
+  const event = await Event.findById(ticket.eventId);
+  if (ticket.onSale) {
+    if (event.status === EventStatus.SOLD_OUT) {
+      await notifyUsers(event.id, event.name);
+      await event.updateOne({
+        $push: { availableTicket: ticket._id },
+        status: EventStatus.ON_SALE,
+      });
+    } else {
+      await event.updateOne({ $push: { availableTicket: ticket._id } });
+    }
+  } else {
+    await event.updateOne({ $pull: { availableTicket: ticket._id } });
+  }
+};
 
 // controllers/event.js
 export const getEventsByUserId = async (req: Request, res: Response) => {
-    const { userId } = req.params;
-    try {
-        const tickets = await Ticket.find({ ownerId: userId }).populate('eventId');
-        const events = tickets.map(ticket => ticket.eventId);
-        res.status(200).json(events);
-    } catch (error) {
-        res.status(500).json({ message: 'Error getting events', error });
-    }
+  const { userId } = req.params;
+  try {
+    const tickets = await Ticket.find({ ownerId: userId }).populate("eventId");
+    const events = tickets.map((ticket) => ticket.eventId);
+    console.log(events);
+    res.status(200).json(events);
+  } catch (error) {
+    res.status(500).json({ message: "Error getting events", error });
+  }
+};
+
+export const getTicketCountByEventId = async (req: Request, res: Response) => {
+  const { userId, eventId } = req.params;
+  console.log(userId, eventId);
+  try {
+    const ticketCount = await Ticket.countDocuments({
+      ownerId: userId,
+      eventId: eventId,
+    });
+    res.json({ ticketCount });
+  } catch (error) {
+    console.error("Error fetching ticket count by event ID:", error);
+    res.status(500).send("Server Error");
+  }
 };
--- src/routes/ticket_route.ts
+++ src/routes/ticket_route.ts
@@ -1,26 +1,34 @@
-import express from 'express';
-import { getTickets, getTicketById,createTicket,updateTicket,deleteTicket, getEventsByUserId } from '../controllers/ticket_controller';
+import express from "express";
+import {
+  getTickets,
+  getTicketById,
+  createTicket,
+  updateTicket,
+  deleteTicket,
+  getEventsByUserId,
+  getTicketCountByEventId,
+} from "../controllers/ticket_controller";
 
 const router = express.Router();
 
-
 // Get all tickets
-router.get('/', getTickets);
+router.get("/", getTickets);
 
 // Get event by ID
-router.get('/:id', getTicketById);
+router.get("/:id", getTicketById);
 
 // Create a new event
-router.post('/', createTicket);
+router.post("/", createTicket);
 
 // Update an existing event
-router.put('/:id', updateTicket);
+router.put("/:id", updateTicket);
 
 // Delete an event
-router.delete('/:id', deleteTicket);
+router.delete("/:id", deleteTicket);
 
 // routes/event.js
-router.get('/user/:userId', getEventsByUserId);
+router.get("/user/:userId", getEventsByUserId);
 
+router.get("/user/:userId/event/:eventId/ticketCount", getTicketCountByEventId);
 
 export default router;
