2ee5b0618f275dd3babbfbeb5afaf06e5278cc81
MatanShabi <46000126+MatanShabi@users.noreply.github.com>
2024-08-06 18:12:18 +0300

    add event search to backend side

--- src/controllers/event.ts
+++ src/controllers/event.ts
@@ -2,83 +2,51 @@ import { Request, Response } from "express";
 import Event, { IEvent } from "../models/event";
 import Ticket from "../models/ticket";
 
-// Get all events
+type EventQuery = {
+    name?: object;
+    type?: object;
+}
+
 export const getEvents = async (req: Request, res: Response) => {
-  try {
-    const events = await Event.find();
-    res.status(200).json(events);
-  } catch (error) {
-    res.status(500).json({ message: "Error getting events", error });
-  }
-};
+    try {
+        let query: EventQuery = {};
 
-// Get event by ID
-export const getEventById = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    const event = await Event.findById(id).populate('availableTicket');
-    if (!event) {
-      return res.status(404).json({ message: "Event not found" });
+        const { q, filter } = req.query;
+
+        if (q) {
+            query.name = { $regex: q, $options: "i" };
+        }
+
+        if (filter) {
+            const filterArray = (filter as string).split(','); 
+            query.type = { $in: filterArray };
+        }
+
+        const events = await Event.find(query);
+
+        res.status(200).json(events);
+    } catch (error) {
+        res.status(500).json({ message: "Error getting events", error });
     }
-    res.status(200).json(event);
-  } catch (error) {
-    res.status(500).json({ message: "Error getting event", error });
-  }
 };
 
-// Create a new event
+
+export const getEventById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const event = await Event.findById(id).populate('availableTicket');
+        if (!event) {
+            return res.status(404).json({ message: "Event not found" });
+        }
+        res.status(200).json(event);
+    } catch (error) {
+        res.status(500).json({ message: "Error getting event", error });
+    }
+};
+
+
 export const createEvent = async (req: Request, res: Response) => {
-  const {
-    name,
-    type,
-    status,
-    description,
-    images,
-    seatmap,
-    startDate,
-    endDate,
-    availableTicket,
-  } = req.body;
-
-  try {
-    const newEvent = new Event({
-      name,
-      type,
-      images,
-      seatmap,
-      description,
-      status,
-      startDate,
-      endDate,
-      availableTicket,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-    await newEvent.save();
-    res.status(201).json(newEvent);
-  } catch (error) {
-    res.status(500).json({ message: "Error creating event", error });
-  }
-};
-
-// Update an existing event
-export const updateEvent = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const {
-    name,
-    type,
-    status,
-    description,
-    images,
-    seatmap,
-    startDate,
-    endDate,
-    availableTicket,
-  } = req.body;
-  try {
-    const updatedEvent = await Event.findByIdAndUpdate(
-      id,
-      {
+    const {
         name,
         type,
         status,
@@ -88,52 +56,102 @@ export const updateEvent = async (req: Request, res: Response) => {
         startDate,
         endDate,
         availableTicket,
-        updatedAt: new Date(),
-      },
-      { new: true }
-    );
-    if (!updatedEvent) {
-      return res.status(404).json({ message: "Event not found" });
+    } = req.body;
+
+    try {
+        const newEvent = new Event({
+            name,
+            type,
+            images,
+            seatmap,
+            description,
+            status,
+            startDate,
+            endDate,
+            availableTicket,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        });
+        await newEvent.save();
+        res.status(201).json(newEvent);
+    } catch (error) {
+        res.status(500).json({ message: "Error creating event", error });
     }
-    res.status(200).json(updatedEvent);
-  } catch (error) {
-    res.status(500).json({ message: "Error updating event", error });
-  }
 };
 
-// Delete an event
-export const deleteEvent = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    const deletedEvent = await Event.findByIdAndDelete(id);
-    if (!deletedEvent) {
-      return res.status(404).json({ message: "Event not found" });
+
+export const updateEvent = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const {
+        name,
+        type,
+        status,
+        description,
+        images,
+        seatmap,
+        startDate,
+        endDate,
+        availableTicket,
+    } = req.body;
+    try {
+        const updatedEvent = await Event.findByIdAndUpdate(
+            id,
+            {
+                name,
+                type,
+                status,
+                description,
+                images,
+                seatmap,
+                startDate,
+                endDate,
+                availableTicket,
+                updatedAt: new Date(),
+            },
+            { new: true }
+        );
+        if (!updatedEvent) {
+            return res.status(404).json({ message: "Event not found" });
+        }
+        res.status(200).json(updatedEvent);
+    } catch (error) {
+        res.status(500).json({ message: "Error updating event", error });
     }
-    res.status(200).json({ message: "Event deleted successfully" });
-  } catch (error) {
-    res.status(500).json({ message: "Error deleting event", error });
-  }
+};
+
+
+export const deleteEvent = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const deletedEvent = await Event.findByIdAndDelete(id);
+        if (!deletedEvent) {
+            return res.status(404).json({ message: "Event not found" });
+        }
+        res.status(200).json({ message: "Event deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Error deleting event", error });
+    }
 };
 
 export const getEventsByUserId = async (req: Request, res: Response) => {
-  const { userId } = req.params;
-  try {
-    const tickets = await Ticket.find({ ownerId: userId }).populate("eventId");
-    const eventMap = new Map<string, IEvent & { ticketCount: number }>();
+    const { userId } = req.params;
+    try {
+        const tickets = await Ticket.find({ ownerId: userId }).populate("eventId");
+        const eventMap = new Map<string, IEvent & { ticketCount: number }>();
 
-    tickets.forEach((ticket) => {
-      const event = ticket.eventId as unknown as IEvent;
-      if (eventMap.has(event._id)) {
-        eventMap.get(event._id)!.ticketCount += 1;
-      } else {
-        eventMap.set(event._id, { ...event, ticketCount: 1 });
-      }
-    });
+        tickets.forEach((ticket) => {
+            const event = ticket.eventId as unknown as IEvent;
+            if (eventMap.has(event._id)) {
+                eventMap.get(event._id)!.ticketCount += 1;
+            } else {
+                eventMap.set(event._id, { ...event, ticketCount: 1 });
+            }
+        });
 
-    const events = Array.from(eventMap.values());
-    console.log(events);
-    res.status(200).json(events);
-  } catch (error) {
-    res.status(500).json({ message: "Error getting events", error });
-  }
+        const events = Array.from(eventMap.values());
+        console.log(events);
+        res.status(200).json(events);
+    } catch (error) {
+        res.status(500).json({ message: "Error getting events", error });
+    }
 };
